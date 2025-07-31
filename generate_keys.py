7f86a3308afeba7f5140fe98dee5f9a5602fac7f
Canberk <canberklarslan@gmail.com>
2025-08-01 00:01:48 +0300

    streamlit-authenticator sürümünü sabitle ve kodu düzelt

---
+++ generate_keys.py
@@ -0,0 +1,11 @@
+import streamlit_authenticator as stauth
+
+# Hash'lenecek şifrelerinizi bu listeye yazın
+passwords_to_hash = ['Uniq2025.', 'IkinciSifre'] 
+
+# Bu sürüm için doğru kullanım:
+# 1. Parola listesini Hasher'a verin
+# 2. .generate() metodunu çağırın
+hashed_passwords = stauth.Hasher(passwords_to_hash).generate()
+
+print(hashed_passwords)
